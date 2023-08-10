537191f2353baf8fb8e017724dc8fbc2974c0008
luiscarmignotto <ikemeneghetti@gmail.com>
2023-08-10 16:34:13 -0300

    Commit to check errors

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions 
+"""
+
+def add(x, y):
+    """ Add x and y and returns result. """
+    return x + y
+
+def subtract(x, y):
+    """ Subtracts x and y and returns result. """
+    return x - y
