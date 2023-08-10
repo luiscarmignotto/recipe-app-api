6630d76c59d26f7c67156897fe96fc49f9b7f82e
luiscarmignotto <ikemeneghetti@gmail.com>
2023-08-10 17:17:22 -0300

    Configured docker-compose with db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
-Calculator functions 
+Calculator functions
 """
+
 
 def add(x, y):
     """ Add x and y and returns result. """
     return x + y
+
 
 def subtract(x, y):
     """ Subtracts x and y and returns result. """
